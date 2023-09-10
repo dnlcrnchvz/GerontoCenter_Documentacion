f94650e794647fcb879b0349df85dc785fc89df9
marinobo <bothimariano2@gmail.com>
2023-09-10 13:47:06 -0600

    modificacion de conexion

--- Centro3raEdad/con_db.php
+++ Centro3raEdad/con_db.php
@@ -1,5 +1,29 @@
 <?php
+//base de datos
+$usuario = "root";
+$password = "";
+$servidor = "localhost";
+$basededatos = "sitioabuelitos";
 
-$conex = mysqli_connect("localhost","root","","sitioabuelitos");
+$conexion = mysqli_connect($servidor,$usuario,"") or die ("Error con el servidor de la base de datos");
+
+$bd = mysqli_select_db($conexion,$basededatos) or die("Error conexion al conectarse a la base de datos");
+
+$name = $_POST['name'];
+$telefono = $_POST['telefono'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO datos VALUES ('$name', '$telefono', '$email')";
+
+$ejecutar = mysql_query($conexion, $sql);
+
+if (!$ejecutar) {
+    # code...
+    echo "Huvo un error";
+}else {
+    echo "Datos guardados correctamente";
+}
+
+
 
 ?>
